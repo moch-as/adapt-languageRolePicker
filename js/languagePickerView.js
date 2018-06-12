395e02e3317d4023181c6764ff5400a6cdb36ada
Lars Halkjær <lh@moch.dk>
2018-06-13 00:42:11 +0200

    using 1.0.6 version code, experimenting with roles

--- js/languagePickerView.js
+++ js/languagePickerView.js
@@ -1,7 +1,7 @@
 define([
     'core/js/adapt',
-    './languagePickerNavigationView'
-], function(Adapt, NavigationView) {
+    './accessibilityView'
+], function(Adapt, accessibilityView) {
     
     var LanguagePickerView = Backbone.View.extend({
         
@@ -20,6 +20,7 @@ define([
         
         render: function () {
             var data = this.model.toJSON();
+            this.adjustForRoles(data);
             var template = Handlebars.templates[this.constructor.template];
             this.$el.html(template(data));
             this.$el.addClass(data._classes);
@@ -41,22 +42,39 @@ define([
         },
 
         initializeAccessibility: function() {
-            this.navigationView = new NavigationView({model:this.model});
+            this.accessibilityView = new accessibilityView({
+                model:this.model
+            });
             
             // we need to re-render if accessibility gets switched on
-            this.listenTo(Adapt, 'accessibility:toggle', this.render);
+            this.listenTo(this.accessibilityView, 'accessibility:toggle', this.render);
         },
 
         destroyAccessibility: function() {
-            this.navigationView.remove();
+            this.accessibilityView.remove();
         },
 
         remove: function() {
             $("html").removeClass("in-languagepicker");
 
             Backbone.View.prototype.remove.apply(this, arguments);
+        },
+
+        adjustForRoles: function(data){
+            if ((data) && (data._languages))
+            {
+                for (var idx = 0; idx < data._languages.length; idx++)
+                {
+                    if ((data._languages[idx]) && (data._languages[idx]._language))
+                    {
+                        var languagename = data._languages[idx]._language;
+                        var languagenameparts = languagename.split('_');
+                        data._languages[idx]._part1 = languagenameparts[0];
+                        data._languages[idx]._part2 = (languagenameparts.length === 2) ? languagenameparts[1] : '';
+                    }
+                }
+            }
         }
-        
     }, {
         template: 'languagePickerView'
     });
